238f4d406a4445bed1cc6d4decea8802e65040f5
javniktool <nikitakremnev0@gmail.com>
2025-03-12 23:08:51 +0300

    add docker and DebugHeaders.php

---
+++ app/Http/Middleware/DebugHeaders.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class DebugHeaders
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Closure(Request): (Response) $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $start = microtime(true);
+        $memory = memory_get_usage();
+
+        $response = $next($request);
+
+        $time = microtime(true) - $start;
+        $memory = memory_get_usage() - $memory;
+
+        $response->header('X-Debug-Time', round($time * 1000, 2) . ' ms');
+        $response->header('X-Debug-Memory', round($memory / 1024, 2) . ' KB');
+
+        return $response;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\GuestController;
+use App\Http\Middleware\DebugHeaders;
 use Illuminate\Support\Facades\Route;
 
-Route::resource('guests', GuestController::class);
+Route::resource('guests', GuestController::class)->middleware(DebugHeaders::class);
